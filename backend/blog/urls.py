d85c39d0a071cf18b5dafd91dc63c1f325390eb4
Aaron Sinnott <aaronsnig@gmail.com>
2021-08-23 19:15:43 +0100

    Replace view with Viewset

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path
-from .views import PostList, PostDetail
+from rest_framework.routers import DefaultRouter
+from .views import PostViewset
 
 app_name = "blog"
 
-urlpatterns = [
-    path("<int:pk>/", PostDetail.as_view(), name="detailcreate"),
-    path("", PostList.as_view(), name="listcreate"),
-]
+router = DefaultRouter()
+router.register("", PostViewset, basename="post")
+urlpatterns = router.urls
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,17 +1,19 @@
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Post
 from .serializers import PostSerializer
 from .permissions import PostUserWritePermission
 
 
-class PostList(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    queryset = Post.post_objects.all()
+class PostViewset(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     serializer_class = PostSerializer
 
+    def get_object(self, queryset=None, **kwargs):
+        item = self.kwargs.get("pk")
+        return get_object_or_404(Post, slug=item)
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
-    permission_classes = [PostUserWritePermission]
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    def get_queryset(self):
+        return Post.objects.all()
+
