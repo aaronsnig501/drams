1555abb49cb280f669a0528365c297fe92421bd1
Aaron Sinnott <aaronsnig@gmail.com>
2021-09-06 20:42:39 +0100

    (refactor): replace tabs with spaces

--- frontend/pages/blog/_slug.vue
+++ frontend/pages/blog/_slug.vue
@@ -1,15 +1,15 @@
 <template>
-	<main>
-		<navigation></navigation>
-		<section class="section">
-			<div class="columns main-section">
+  <main>
+    <navigation></navigation>
+    <section class="section">
+      <div class="columns main-section">
         <div class="column is-2"></div>
         <div class="column is-8">
-					<h1 class="title is-2">{{ data.title }}</h1>
-				</div>
-			</div>
-		</section>
-	</main>
+          <h1 class="title is-2">{{ data.title }}</h1>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
