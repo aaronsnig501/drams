acf8d001073b79e3946d55478c99cb3c2c424103
Aaron Sinnott <aaronsnig@gmail.com>
2021-10-11 18:13:45 +0100

    (fix): resolve pagination issues

--- backend/whiskey/views.py
+++ backend/whiskey/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.filters import SearchFilter
 from .models import Brand, Whiskey
 from .serializers import BrandSerializer, WhiskeySerializer
@@ -41,6 +40,7 @@ class WhiskeyViewSet(ModelViewSet):
             chill_filtered_param = Whiskey.CHILL_FILTERED[int(
                 params["chill_filtered"])][0]
 
+        name_param = params.get("name", None)
         proof_param = params.get("proof", None)
 
         params = {
@@ -58,15 +58,4 @@ class WhiskeyViewSet(ModelViewSet):
     def get_queryset(self):
         params = self._parse_query_params()
         return Whiskey.objects.all().filter(**params).order_by(
-            self.request.GET["ordering"])
-
-    def list(self, request):
-        queryset = self.get_queryset()
-        page = self.paginate_queryset(queryset)
-
-        if page is not None:
-            serializer = self.serializer_class(page, many=True)
-            return self.get_paginated_response(serializer.data)
-        else:
-            serializer = self.serializer_class(queryset, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            self.request.GET.get("ordering", "-name"))
